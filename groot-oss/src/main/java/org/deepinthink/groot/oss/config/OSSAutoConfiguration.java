ed7a4419fa85c43ade3717985559732072399204
Tingyan Shen <tingyan.shen@gmail.com>
2021-07-01 15:15:39 +0800

    Add cloud OSS facade template and driver configuration

--- groot-oss/src/main/java/org/deepinthink/groot/oss/config/OSSAutoConfiguration.java
+++ groot-oss/src/main/java/org/deepinthink/groot/oss/config/OSSAutoConfiguration.java
@@ -15,9 +15,41 @@
  */
 package org.deepinthink.groot.oss.config;
 
+import org.deepinthink.groot.aliyun.config.AliyunOSSAutoConfiguration;
+import org.deepinthink.groot.aws.config.AmazonS3AutoConfiguration;
+import org.deepinthink.groot.gcloud.config.GCloudStorageAutoConfiguration;
+import org.deepinthink.groot.minio.config.MinioAutoConfiguration;
+import org.deepinthink.groot.oss.aliyun.AliyunOSSConfiguration;
+import org.deepinthink.groot.oss.aws.AmazonS3Configuration;
+import org.deepinthink.groot.oss.gcloud.GCloudStorageConfiguration;
+import org.deepinthink.groot.oss.minio.MinioConfiguration;
+import org.deepinthink.groot.oss.qcloud.QCloudCOSConfiguration;
+import org.deepinthink.groot.oss.ucloud.UCloudUFileConfiguration;
+import org.deepinthink.groot.qcloud.config.QCloudCOSAutoConfiguration;
+import org.deepinthink.groot.ucloud.config.UCloudUFileAutoConfiguration;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 
 @SpringBootConfiguration(proxyBeanMethods = false)
 @EnableConfigurationProperties(OSSProperties.class)
+@AutoConfigureAfter({
+  AliyunOSSAutoConfiguration.class,
+  AmazonS3AutoConfiguration.class,
+  org.deepinthink.groot.aws.v2.config.AmazonS3AutoConfiguration.class,
+  GCloudStorageAutoConfiguration.class,
+  MinioAutoConfiguration.class,
+  QCloudCOSAutoConfiguration.class,
+  UCloudUFileAutoConfiguration.class
+})
+@Import({
+  AliyunOSSConfiguration.class,
+  AmazonS3Configuration.class,
+  org.deepinthink.groot.oss.aws.v2.AmazonS3Configuration.class,
+  GCloudStorageConfiguration.class,
+  MinioConfiguration.class,
+  QCloudCOSConfiguration.class,
+  UCloudUFileConfiguration.class
+})
 public class OSSAutoConfiguration {}
